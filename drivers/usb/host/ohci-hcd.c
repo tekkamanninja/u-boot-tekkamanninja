477887ef6ead431d29b4fd43da2eb97511062d55
Tekkaman Ninja <tekkamanninja@gmail.com>
2011-12-16 23:07:30 +0800

    add CONFIG_S3C6410 for mini6410

--- drivers/usb/host/ohci-hcd.c
+++ drivers/usb/host/ohci-hcd.c
@@ -67,6 +67,7 @@
 #if defined(CONFIG_ARM920T) || \
     defined(CONFIG_S3C24X0) || \
     defined(CONFIG_S3C6400) || \
+    defined(CONFIG_S3C6410) || \
     defined(CONFIG_440EP) || \
     defined(CONFIG_PCI_OHCI) || \
     defined(CONFIG_MPC5200) || \
